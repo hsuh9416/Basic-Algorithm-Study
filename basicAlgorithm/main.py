ad53e519815ce692e25bae3776d0ba8e633ec4d4
DESKTOP-TRLOEQJ\cetun <suhhaeun87@gmail.com>
2022-05-01 21:48:05 -0500

    Added test script

---
+++ basicAlgorithm/main.py
@@ -0,0 +1,26 @@
+# Test script Ver 1.0
+from datetime import datetime
+
+
+def current_dt():
+    """
+    Function returns current datetime as "mm/dd/yyyy HH:MM:SS" format
+    :return: String of formatted datetime
+    """
+    return datetime.now().strftime("%m/%d/%y %H:%M:%S")
+
+
+def greeting(target_dt):
+    """
+    Function prints greeting sentences
+    :param target_dt: String of formatted datetime
+    :return: None
+    """
+    print(f"Hi, there! This is test script running!")
+    print(f"Current datetime is {target_dt}")
+    print("I hope you enjoy your day!")
+    print("HAPPY CODING:)")
+
+
+if __name__ == "__main__":
+    greeting(current_dt())
