6e3a071f36d05876717c8bb5bc651fb487f63e4a
DESKTOP-TRLOEQJ\cetun <suhhaeun87@gmail.com>
2022-05-04 20:19:31 -0500

    Add function testing sentinel method

--- basicAlgorithm/ch3.py
+++ basicAlgorithm/ch3.py
@@ -26,7 +26,15 @@ def seq_search(a: Sequence, key: Any) -> Any:  # Simple linear Search
     return None
 
 
-def seq_search_test():  # Simple linear Search Test
+def seq_search_advanced(a: list, key: Any) -> Any:  # Sentinel method applied
+    li = a[:] + [key]  # To avoid modification!
+    i = 0
+    for i in range(len(li)):
+        if li[i] == key:
+            return i
+
+
+def get_list_key():  # General function to create list and key
     li = rand_list()
     ky = None
 
@@ -36,6 +44,11 @@ def seq_search_test():  # Simple linear Search Test
             ky = temp
         except ValueError:
             print('Enter integer number only!')
+    return li, ky
+
+
+def seq_search_test():  # Simple linear Search Test
+    li, ky = get_list_key()
     result = seq_search(li, ky)
 
     if result is None:
@@ -44,5 +57,16 @@ def seq_search_test():  # Simple linear Search Test
         print(f'Index Number of number {ky} from the list = {result}')
 
 
+def sentinel_method_test():  # sentinel method applied Test
+    li, ky = get_list_key()
+    result = seq_search_advanced(li, ky)
+
+    if result == len(li):
+        print(f'Number {ky} does not exists!')
+    else:
+        print(f'Index Number of number {ky} from the list = {result}')
+
+
 if __name__ == '__main__':
-    seq_search_test()
+    # seq_search_test()
+    sentinel_method_test()
