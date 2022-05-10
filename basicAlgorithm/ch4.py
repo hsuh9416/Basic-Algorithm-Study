7ae2fba1cab7e4dc95b23a6f3ee57545e9005d29
DESKTOP-TRLOEQJ\cetun <suhhaeun87@gmail.com>
2022-05-09 20:12:50 -0500

    Update& Ended up ch4(04-2)

--- basicAlgorithm/ch4.py
+++ basicAlgorithm/ch4.py
@@ -20,7 +20,19 @@
         => obj.__contains__(x) --> x in obj
     5) collection.deque
         => One of the Python built-in modules/libraries
-        => The module enables to add/delete the element bidirectionally from the que
+        => The module enables to add/delete the element bidirectionally from the sequence
+    6) Notation for queue:
+        a> enqueue: Add data
+        b> dequeue: Remove data
+        c> front: Direction from which removes data
+        d> rear: Direction from which adds data
+        e> priority queue: Higher priority data will be deleted first
+            => Priority will be granted while adding data and data will be removed from the highest priority
+            => Python module 'heapq' enables granting priority to queue
+        f> ring buffer: The queue type that does not move element within array when dequeue executed
+            => The front/rear are logical order but not physical order
+            => The front/rear will be updated when enqueue/dequeue executed
+            => The complexity of ring buffer executions are all the same as O(1)
 
 """
 from typing import Any
@@ -28,7 +40,9 @@ from ch3 import select_menu
 from enum import Enum
 from collections import deque
 
-Menu = Enum('Menu', ['Push', 'Pop', 'Peak', 'Find', 'Dump', 'Terminate'])
+Menu_stack = Enum('Menu', ['Push', 'Pop', 'Peak', 'Find', 'Dump', 'Terminate'])
+
+Menu_queue = Enum('Menu', ['Enqueue', 'Dequeue', 'Peak', 'Find', 'Dump', 'Terminate'])
 
 
 def try_statement_test() -> None:
@@ -152,32 +166,152 @@ class Stack:
         print(list(self.__stk))
 
 
-def run_menu(s: Any) -> None:
+class FixedQueue:
+
+    class Empty(Exception):  # Subclass of both FixedStack and Exception
+        pass
+
+    class Full(Exception):  # Subclass of both FixedStack and Exception
+        pass
+
+    def __init__(self, capacity: int) -> None:
+        self.no = 0  # Current number of data
+        self.front = 0  # First element cursor
+        self.rear = 0  # Last element cursor
+        self.capacity = capacity  # Size of queue
+        self.que = [None]*capacity  # Queue object(list)
+
+    def __len__(self) -> int:
+        return self.no
+
+    def is_empty(self) -> bool:
+        return self.no <= 0
+
+    def is_full(self) -> bool:
+        return self.no >= self.capacity
+
+    def enqueue(self, x: Any) -> None:
+        if self.is_full():
+            raise FixedQueue.Full
+        self.que[self.rear] = x
+        self.rear += 1
+        self.no += 1
+        if self.rear == self.capacity:
+            self.rear = 0
+
+    def dequeue(self) -> Any:
+        if self.is_empty():
+            raise FixedQueue.Empty
+        x = self.que[self.front]
+        self.front += 1
+        self.no -= 1
+        if self.front == self.capacity:
+            self.front == self.capacity
+        return x
+
+    def peek(self) -> Any:
+        if self.is_empty():
+            raise FixedQueue.Empty
+        return self.que[self.front]
+
+    def find(self, value: Any) -> Any:
+        for i in range(self.no):
+            idx = (i + self.front) % self.capacity  # inorder to rotate properly
+            if self.que[idx] == value:
+                return idx
+        return -1
+
+    def count(self, value: Any) -> bool:
+        c = 0
+        for i in range(self.no):
+            idx = (i + self.front) % self.capacity  # inorder to rotate properly
+            if self.que[idx] == value:
+                c += 1
+        return c
+
+    def __contains__(self, value: Any) -> bool:
+        return self.count(value)
+
+    def clear(self) -> None:
+        self.no = self.front = self.rear = 0  #
+
+    def dump(self) -> None:
+        if self.is_empty():
+            print('Queue is Empty!')
+        else:
+            for i in range(self.no):
+                print(self.que[(i + self.front) % self.capacity], end='')
+                print()
+
+
+def run_stack_menu(s: Any) -> None:
     while True:
         print(f'Current number of data: {len(s)} / {s.capacity}')
-        menu = select_menu(Menu)
+        menu = select_menu(Menu_stack)
         try:
-            if menu == Menu.Push:  # Add to the stack
+            if menu == Menu_stack.Push:  # Add to the stack
                 x = int(input('Enter the key to add: '))
                 s.push(x)
 
-            elif menu == Menu.Pop:  # Remove from the stack and return the value removed
+            elif menu == Menu_stack.Pop:  # Remove from the stack and return the value removed
                 s.pop()
                 print(f'Popped data: {x}')
 
-            elif menu == Menu.Peak:  # Return the latest stacked value
+            elif menu == Menu_stack.Peak:  # Return the latest stacked value
                 x = s.peek()
                 print(f'Peeked data: {x}')
 
-            elif menu == Menu.Find:  # Return the target value
+            elif menu == Menu_stack.Find:  # Return the target value
                 x = int(input('Enter the value to search: '))
                 if x in s:
                     print(f'The first index found: {s.find(x)}, Count: {s.count(x)}')
                 else:
                     print(f'The value "{x}" does not exist!')
 
-            elif menu == Menu.Dump:  # Show the stack
+            elif menu == Menu_stack.Dump:  # Show the stack
                 s.dump()
+
+            else:
+                break
+
+        except ValueError:
+            print('Please only enter integer numbers!!')
+        except FixedStack.Full:
+            print('The stack is full!!')
+        except FixedStack.Empty:
+            print('The stack is empty!!')
+        else:
+            print('The process done successful')
+        finally:
+            print('Return to the selection')
+
+
+def run_queue_menu(q: FixedQueue) -> None:
+    while True:
+        print(f'Current number of data: {len(q)} / {q.capacity}')
+        menu = select_menu(Menu_queue)
+        try:
+            if menu == Menu_queue.Enqueue:  # Add to the stack
+                x = int(input('Enter the data to enqueue: '))
+                q.enqueue(x)
+
+            elif menu == Menu_queue.Dequeue:  # Remove from the stack and return the value removed
+                q.dequeue()
+                print(f'Dequeued data: {x}')
+
+            elif menu == Menu_queue.Peak:  # Return the latest stacked value
+                x = q.peek()
+                print(f'Peeked data: {x}')
+
+            elif menu == Menu_queue.Find:  # Return the target value
+                x = int(input('Enter the value to search: '))
+                if x in q:
+                    print(f'The first index found: {q.find(x)}, Count: {q.count(x)}')
+                else:
+                    print(f'The value "{x}" does not exist!')
+
+            elif menu == Menu_queue.Dump:  # Show the stack
+                q.dump()
 
             else:
                 break
@@ -196,15 +330,21 @@ def run_menu(s: Any) -> None:
 
 def fixed_stack_test():
     s = FixedStack(64)  # capacity = 64
-    run_menu(s)
+    run_stack_menu(s)
 
 
 def deque_stack_test():
     s = Stack(64)  # capacity = 64
-    run_menu(s)
+    run_stack_menu(s)
+
+
+def queue_test():
+    q = FixedQueue(64)  # capacity = 64
+    run_queue_menu(q)
 
 
 if __name__ == '__main__':
     try_statement_test()
     fixed_stack_test()
     deque_stack_test()
+    queue_test()
