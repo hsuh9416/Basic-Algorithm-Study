5814b2f2fbc533a4ed6b7f4c6dd1a457c6be00ee
DESKTOP-TRLOEQJ\cetun <suhhaeun87@gmail.com>
2022-05-18 22:43:37 -0500

    Update ch8(08-1 ~ 08-2) * 08-2 is not finished

--- basicAlgorithm/ch8.py
+++ basicAlgorithm/ch8.py
@@ -14,7 +14,7 @@
 
 """
 from __future__ import annotations
-from typing import Any, Type
+from typing import Any
 
 
 class Node:
@@ -35,6 +35,98 @@ class LinkedList:
 
     def __len__(self) -> int:
         return self.no
+
+    def search(self, data: Any) -> int:
+        cnt = 0
+        ptr = self.head
+        while ptr is not None:
+            if ptr.data == data:
+                self.current = ptr
+                return cnt
+            cnt += 1
+            ptr = ptr.next
+        return -1
+
+    def __contains__(self, data: Any) -> bool:
+        return self.search(data) >= 0
+
+    def add_first(self, data: Any) -> None:
+        ptr = self.head
+        self.head = self.current = Node(data, ptr)  # Set original head node to next node than store
+        self.no += 1
+
+    def add_last(self, data: Any) -> None:
+        if self.head is None:
+            self.add_first(data)
+        else:
+            ptr = self.head
+            while ptr.successor is not None:  # Proceed until the end
+                ptr = ptr.successor
+            ptr.successor = self.current = Node(data, None)  # Add node to last
+            self.no += 1
+
+    def remove_first(self) -> None:
+        if self.head is not None:
+            self.head = self.current = self.head.successor
+            self.no -= 1
+
+    def remove_last(self):
+        if self.head is not None:
+            if self.head.successor is None:
+                self.remove_first()
+            else:
+                ptr = self.head
+                pre = self.head
+
+                while ptr.successor is not None:
+                    pre = ptr
+                    ptr = ptr.successor
+                pre.successor = None  # Set the second last node to the last node
+                self.current = pre
+                self.no -= 1
+
+    def remove(self, p: Node) -> None:
+        if self.head is not None:
+            if p is self.head:
+                self.remove_first()
+            else:
+                ptr = self.head
+
+                while ptr.successor is not p:
+                    ptr = ptr.successor
+                    if ptr is None:
+                        return  # ptr was not exist
+                    ptr.successor = p.successor
+                    self.current = ptr
+                    self.no -= 1
+
+    def remove_current_node(self) -> None:
+        self.remove(self.current)
+
+    def clear(self) -> None:
+        while self.head is not None:
+            self.remove_first()
+        self.current = None
+        self.no = 0
+
+    def next(self) -> bool:
+        if self.current is None or self.current.successor is None:
+            return False
+        self.current = self.current.successor
+        return True
+
+    def print_current_node(self) -> None:
+        if self.current is None:
+            print('No current node exists!')
+        else:
+            print(self.current.data)
+
+    def print(self) -> None:
+        ptr = self.head
+
+        while ptr is None:
+            print(ptr.data)
+            ptr = ptr.next
 
 
 if __name__ == '__main__':
