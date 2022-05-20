423dffea521f216b158c326b151e9348d53a1b3e
DESKTOP-TRLOEQJ\cetun <suhhaeun87@gmail.com>
2022-05-20 14:54:10 -0500

    Update and ch8(08-2) * End up 08-2

--- basicAlgorithm/ch8.py
+++ basicAlgorithm/ch8.py
@@ -15,6 +15,16 @@
 """
 from __future__ import annotations
 from typing import Any
+from enum import Enum
+from ch3 import select_menu
+
+LinkedList_Menu = Enum('LinkedList_Menu',
+                       ['INSERT_FIRST', 'INSERT_LAST', 'DELETE_HEAD',
+                        'DELETE_TAIL', 'PRINT_CURRENT', 'MOVE_NEXT',
+                        'DELETE_CURRENT', 'CLEAR_ALL', 'SEARCH', 'IS_PRESENT',
+                        'PRINT_ALL', 'SCAN', 'TERMINATE'
+                        ]
+                       )
 
 
 class Node:
@@ -44,7 +54,7 @@ class LinkedList:
                 self.current = ptr
                 return cnt
             cnt += 1
-            ptr = ptr.next
+            ptr = ptr.successor
         return -1
 
     def __contains__(self, data: Any) -> bool:
@@ -52,7 +62,7 @@ class LinkedList:
 
     def add_first(self, data: Any) -> None:
         ptr = self.head
-        self.head = self.current = Node(data, ptr)  # Set original head node to next node than store
+        self.head = self.current = Node(data, ptr)  # Set head node to new node then link to the old head node
         self.no += 1
 
     def add_last(self, data: Any) -> None:
@@ -67,7 +77,7 @@ class LinkedList:
 
     def remove_first(self) -> None:
         if self.head is not None:
-            self.head = self.current = self.head.successor
+            self.head = self.current = self.head.successor  # Set Original head node to next node then store
             self.no -= 1
 
     def remove_last(self):
@@ -78,33 +88,33 @@ class LinkedList:
                 ptr = self.head
                 pre = self.head
 
-                while ptr.successor is not None:
+                while ptr.successor is not None:  # Proceed until the end
                     pre = ptr
                     ptr = ptr.successor
                 pre.successor = None  # Set the second last node to the last node
                 self.current = pre
                 self.no -= 1
 
-    def remove(self, p: Node) -> None:
+    def remove(self, p: Node) -> None:  # Remove intended node
         if self.head is not None:
             if p is self.head:
                 self.remove_first()
             else:
                 ptr = self.head
 
-                while ptr.successor is not p:
+                while ptr.successor is not p:  # Stop loop when ptr.successor == p
                     ptr = ptr.successor
                     if ptr is None:
                         return  # ptr was not exist
-                    ptr.successor = p.successor
-                    self.current = ptr
-                    self.no -= 1
+                ptr.successor = p.successor  # Make to skip p itself
+                self.current = ptr
+                self.no -= 1
 
     def remove_current_node(self) -> None:
         self.remove(self.current)
 
     def clear(self) -> None:
-        while self.head is not None:
+        while self.head is not None:  # Proceed removing one by one
             self.remove_first()
         self.current = None
         self.no = 0
@@ -126,8 +136,83 @@ class LinkedList:
 
         while ptr is not None:
             print(ptr.data)
-            ptr = ptr.next
+            ptr = ptr.successor
+
+    def __iter__(self) -> LinkedListInterator:
+        return LinkedListInterator(self.head)
+
+
+class LinkedListInterator:
+
+    def __init__(self, head: Node):
+        self.current = head
+
+    def __iter__(self) -> LinkedListInterator:
+        return self
+
+    def __next__(self) -> Any:
+        if self.current is None:
+            raise StopIteration
+        else:
+            data = self.current.data
+            self.current = self.current.successor  # Presetting before future next() call
+            return data
+
+
+def run_menu(lst: LinkedList) -> None:
+    while True:
+        menu = select_menu(LinkedList_Menu)
+
+        if menu == LinkedList_Menu.INSERT_FIRST:  # Add to head node
+            lst.add_first(input('Enter the value to add at head node: '))
+
+        elif menu == LinkedList_Menu.INSERT_LAST:  # Add to tail node
+            lst.add_last(input('Enter the value to add at tail node: '))
+
+        elif menu == LinkedList_Menu.DELETE_HEAD:  # Remove current head node
+            lst.remove_first()
+
+        elif menu == LinkedList_Menu.DELETE_TAIL:  # Remove current tail node
+            lst.remove_last()
+
+        elif menu == LinkedList_Menu.PRINT_CURRENT:  # Print current node
+            lst.print_current_node()
+
+        elif menu == LinkedList_Menu.MOVE_NEXT:  # Move to the next node
+            lst.next()
+
+        elif menu == LinkedList_Menu.DELETE_CURRENT:  # Remove current node
+            lst.remove_current_node()
+
+        elif menu == LinkedList_Menu.CLEAR_ALL:  # Remove all node
+            lst.clear()
+
+        elif menu == LinkedList_Menu.SEARCH:  # Search the intended value
+            pos = lst.search(input('Enter the value to search: '))
+            if pos >= 0:
+                print(f'The index of the data: {pos}')
+            else:
+                print('The data is not exists!')
+
+        elif menu == LinkedList_Menu.IS_PRESENT:  # Check the value is present
+            answer = input('Enter the value to check if present: ')
+            print(f'Result: {"Exists" if answer in lst else "Not Exists"}')
+
+        elif menu == LinkedList_Menu.PRINT_ALL:  # Show all values in the list
+            lst.print()
+
+        elif menu == LinkedList_Menu.SCAN:  # Show the data one by one
+            for e in lst:
+                print(e)
+
+        else: # End the menu
+            break
+
+
+def linkedlist_test():
+    lst = LinkedList()
+    run_menu(lst)
 
 
 if __name__ == '__main__':
-    print('To be continued')
+    linkedlist_test()
