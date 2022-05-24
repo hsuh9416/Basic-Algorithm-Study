a066f9cd88f4f367283ac6469aade328b07471e8
DESKTOP-TRLOEQJ\cetun <suhhaeun87@gmail.com>
2022-05-24 09:50:25 -0500

    Created Project BasicAlgorithmJava

---
+++ BasicAlgorithmJava/src/main/java/com/example/basicalgorithmjava/BasicAlgorithmJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicalgorithmjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAlgorithmJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicAlgorithmJavaApplication.class, args);
+    }
+
+}
---
+++ BasicAlgorithmJava/src/test/java/com/example/basicalgorithmjava/BasicAlgorithmJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicalgorithmjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAlgorithmJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
