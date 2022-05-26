7b38db575a828a2a103d59ad85cdd050e86ae0b0
DESKTOP-TRLOEQJ\cetun <suhhaeun87@gmail.com>
2022-05-25 20:56:47 -0500

    Modify Java project

--- basic_algorithm_java/src/main/java/com/basicalgorithm/BasicAlgorithmJavaApplication.java
+++ basic_algorithm_java/src/main/java/com/basicalgorithm/BasicAlgorithmJavaApplication.java
@@ -1,4 +1,4 @@
-package com.example.basicalgorithmjava;
+package com.basicalgorithm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
