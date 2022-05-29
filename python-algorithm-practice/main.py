60f27faac42a4831cd7491f773edb270271e569c
DESKTOP-TRLOEQJ\cetun <suhhaeun87@gmail.com>
2022-05-29 16:37:20 -0500

    Added test code in project

--- python-algorithm-practice/main.py
+++ python-algorithm-practice/main.py
@@ -1,16 +1,26 @@
-# 샘플 Python 스크립트입니다.
-
-# Shift+F10을(를) 눌러 실행하거나 내 코드로 바꿉니다.
-# 클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 Shift 두 번을(를) 누릅니다.
+# Test script Ver 1.0
+from datetime import datetime
 
 
-def print_hi(name):
-    # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
-    print(f'Hi, {name}')  # 중단점을 전환하려면 Ctrl+F8을(를) 누릅니다.
+def current_dt():
+    """
+    Function returns current datetime as "mm/dd/yyyy HH:MM:SS" format
+    :return: String of formatted datetime
+    """
+    return datetime.now().strftime("%m/%d/%y %H:%M:%S")
 
 
-# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def greeting(target_dt):
+    """
+    Function prints greeting sentences
+    :param target_dt: String of formatted datetime
+    :return: None
+    """
+    print(f"Hi, there! This is test script for 'python-algorithm-practice' running!")
+    print(f"Current datetime is {target_dt}")
+    print("I hope you enjoy your day!")
+    print("HAPPY CODING:)")
 
-# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
+
+if __name__ == "__main__":
+    greeting(current_dt())
